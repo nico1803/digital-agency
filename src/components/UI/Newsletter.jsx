f4586c77aebdc5c83cbc47c4fc57693f8b54daba
Nicolas Beovide <nico1803o@hotmail.com.ar>
2023-05-10 06:57:46 -0300

    feat ( Newsletter ) component + css

---
+++ src/components/UI/Newsletter.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import '../../styles/newsletter.css'
+
+export default function Newsletter() {
+  return (
+    <section className="newsletter">
+        <div className="container">
+            <div className="newsletter__wrapper">
+                <div className="newsletter__content">
+                    <h6 className="subtitle">Let's work</h6>
+                    <h2>Explore the <span className="highlight">hidden</span> ideas and subscibe!</h2>
+                </div>
+
+                <div className="newsletter__form">
+                    <input type="email" placeholder='Email'/>
+                    <button className="secondary__btn">Subscribe Now</button>
+                </div>
+
+            </div>
+        </div>
+    </section>
+  )
+}
+
